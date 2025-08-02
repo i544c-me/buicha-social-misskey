d880247253c2ef846d1a9a83673baf9760b25426
anatawa12 <anatawa12@icloud.com>
2025-08-02 14:58:27 +0900

    add spdx license text

--- packages/frontend/src/utility/image-compression.ts
+++ packages/frontend/src/utility/image-compression.ts
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 
 export const imageCompressionTargetSizes = [
 	1125, // original misskey compression mode = 3 (high)
